121655c397103db3b1754ac7d85f95ed7d8dfdf4
ribeiroyoann <yoribeir@student.42.fr>
2020-02-19 17:17:13 +0100

    1

---
+++ src/components/intro.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./intro.module.css"
+
+export default () => (
+  <div>
+    <p className={styles.underline}>Hello, I'm</p>
+    <h2 className={styles.headline}>Yoann Ribeiro</h2>
+    <p className={styles.underline}>Full stack developer student</p>
+  </div>
+)
